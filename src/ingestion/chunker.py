ba333ae995ae93e1d574668e805899ed70386629
Samarth <32644389+samy1406@users.noreply.github.com>
2026-05-14 07:43:39 +0000

    add the logic to chunker using RecursiveCharacterTextSplitter for smart chunking

--- src/ingestion/chunker.py
+++ src/ingestion/chunker.py
@@ -1,2 +1,18 @@
-def chunker():
-    pass
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+def chunk_text(text, chunk_size=512, chunk_overlap=50):
+
+    # check if the text is not empty
+    if not text or not text.strip():
+        return []
+
+    # creating the splitter to split the chunks
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = chunk_size,
+        chunk_overlap = chunk_overlap,
+        separators=["\n\n", "\n", " ", ""]
+    )
+    
+    # split using spliter created
+    return splitter.split_text(text)
