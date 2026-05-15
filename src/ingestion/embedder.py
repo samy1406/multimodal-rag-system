2e3fe23056b6e9270459373ba061db6598f1b16e
Samarth <32644389+samy1406@users.noreply.github.com>
2026-05-15 06:26:57 +0000

    add the embedder code to use CLIP to embed both image and text and return in same dimension

--- src/ingestion/embedder.py
+++ src/ingestion/embedder.py
@@ -1,2 +1,26 @@
-def embedder():
-    pass
+import clip
+from PIL import Image
+import torch
+import numpy as np
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+model, preprocess = clip.load("ViT-B/32", device=device)
+# uses the sentence transformer
+def embed_text(texts: list[str]) -> np.ndarray:
+    tokens = clip.tokenize(texts).to(device)
+    text_feature = model.encode_text(tokens)
+    text_feature /= text_feature.norm(dim=-1, keepdim=True)
+    text_feature = text_feature.detach().cpu().numpy()
+    return text_feature
+
+# uses CLIP embedding
+def embed_image(pil_images: list) -> np.ndarray:
+    image_features = []
+    for i in range(len(pil_images)):
+        image = preprocess(pil_images[i]).unsqueeze(0).to(device)
+        image_feature = model.encode_image(image)
+        image_feature /= image_feature.norm(dim=-1, keepdim=True)
+        image_feature = image_feature.detach().cpu().numpy()
+        image_features.append(image_feature)
+    return np.vstack(image_features)
