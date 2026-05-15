2697fb76986ee47fdb3baa81c0d171ee5506ccc9
Samarth <32644389+samy1406@users.noreply.github.com>
2026-05-15 07:25:59 +0000

    add the captioner code to use BLIP to caption

--- src/ingestion/captioner.py
+++ src/ingestion/captioner.py
@@ -1,2 +1,18 @@
-def captioner():
-    pass
+from transformers import BlipProcessor, BlipForConditionalGeneration
+
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+def generate_caption(pil_image) -> str:
+
+    input = processor(images=pil_image, return_tensors="pt")
+
+    output_ids = model.generate(**input)
+
+    caption = processor.decode(output_ids[0], skip_special_tokens=True)
+
+    return caption
+
+# processor converts PIL image → tensor done
+# model generates token ids done
+# processor decodes token ids → string done
