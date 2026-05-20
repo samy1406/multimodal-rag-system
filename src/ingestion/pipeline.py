d827bb92211475b11f3186a6e92e14839a55f684
Samarth <32644389+samy1406@users.noreply.github.com>
2026-05-20 07:55:22 +0000

    implement ingestion pipeline glue for all the module

---
+++ src/ingestion/pipeline.py
@@ -0,0 +1,53 @@
+#pdf_loader → chunker (text) → embedder → captioner (images) → metadata_store → index_builder
+import pdf_loader
+import chunker
+import embedder
+import captioner
+import metadata_store
+import retrieval.index_builder as index_builder
+import numpy as np
+from PIL import Image
+
+
+def run_pipeline(pdf_path):
+    pages = pdf_loader.load_pdf(pdf_path)
+    
+    all_vectors = []
+    all_metadata = {}
+    current_id = 0
+    
+    for page in pages:
+        # TEXT
+        if page["text"].strip():
+            chunks = chunker.chunk_text(page["text"])
+            vectors = embedder.embed_text(chunks)
+            for i, chunk in enumerate(chunks):
+                all_vectors.append(vectors[i])
+                all_metadata[current_id] = {
+                    "type": "text",
+                    "source": pdf_path,
+                    "page": page["page_number"],
+                    "content": chunk,
+                    "image_path": None
+                }
+                current_id += 1
+        
+        # IMAGES
+        for img in page["images"]:
+            caption = captioner.generate_caption(img)
+            vector = embedder.embed_image([img])
+            all_vectors.append(vector[0])
+            all_metadata[current_id] = {
+                "type": "image",
+                "source": pdf_path,
+                "page": page["page_number"],
+                "content": caption,
+                "image_path": pdf_path + "_page_" + str(page["page_number"])
+            }
+            current_id += 1
+    
+    # build + save index
+    all_vectors = np.vstack(all_vectors)
+    index = index_builder.build_index(all_vectors)
+    index_builder.save_index(index, "/data/index/")
+    metadata_store.save_metadata(all_metadata, "metadata.json")
